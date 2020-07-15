86d769a4afaca5d3eff7dd77e39dda345aa673ad
Bijay Babu Regmi <stw0010@softwarica.edu.np>
2020-07-15 09:35:48 +0545

    try

---
+++ test9.py
@@ -0,0 +1,8 @@
+for i in range(1,11):
+    for j in range(1,i+1):
+        print("*", end = " ")
+    print()
+for i in range(9,0,-1):
+    for j in range(1, i+1):
+        print("*", end = " ")
+    print()
